1618d1b3620c490cab072c60c2a111510c814b4a
Chris Crump <chris@cypheros.co>
2020-06-02 00:15:55 -0400

    op8: Configure round corner padding.
    Change-Id: I6d0675109e0f2a72f65edc4d704c2b16ad21172e

--- device.mk
+++ device.mk
@@ -106,9 +106,11 @@ PRODUCT_PACKAGES += \
     AOSPAOnePlus8SeriesSystemUI \
     OnePlus8Frameworks \
     OnePlus8ProFrameworks \
+    OnePlus8ProSystemUI \
     OnePlus8SeriesBluetooth \
     OnePlus8SeriesCarrierConfig \
-    OnePlus8SeriesFrameworks
+    OnePlus8SeriesFrameworks \
+    OnePlus8SystemUI
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
