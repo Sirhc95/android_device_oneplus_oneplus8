6ecdb2f952008e85358828b6ff36b0f8caff1fe2
Chris Crump <chris@cypheros.co>
2020-06-19 23:45:17 -0400

    oneplus8: Import WCNSS_qcom_cfg from OOS
    From OnePlus8Pro-user-10-QKQ1.191222.002-2005100226-release-keys

--- device.mk
+++ device.mk
@@ -144,5 +144,9 @@ PRODUCT_PACKAGES += \
     update_engine_sideload \
     update_verifier
 
+# WiFi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/wifi/WCNSS_qcom_cfg.ini
+
 # Vendor
 $(call inherit-product, vendor/oneplus/oneplus8/oneplus8-vendor.mk)
